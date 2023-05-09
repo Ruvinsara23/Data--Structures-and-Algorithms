c802a72f8e434fb57d1e5cfa4bb67220bb204094
Ruvinsara23 <101954975+Ruvinsara23@users.noreply.github.com>
2023-05-09 02:33:40 +0000

    Big O

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-// What is the Big O of the below function? (Hint, you may want to go line by line)
-function funChallenge(input) {
-  let a = 10;
-  a = 50 + 3;
+//Big O notation
 
-  for (let i = 0; i < input.length; i++) {
-    anotherFunction();
-    let stranger = true;
-    a++;
-  }
-  return a;
+//O(n)-Liner time 
+
+console.log('Hello');
+const arr=['nemo'];
+
+function findNemo(array){
+   for(let i=0; i<array.length; i++){
+    if (array[i]==='nemo'){
+        console.log(array[i]);
+    }
+    else{
+        console.log(array[i]);
+    } 
+   }
 }
+
+findNemo(arr);
+
+// O(1)=const time 
+
+// No matter how many  inputs are only run certain number of operation
+const boxes=[1,2,4,6,78,7];
+
+const logFirstTwoBoxes=(boxes)=>{
+    console.log(boxes[0]);//0(1)
+    console.log(boxes[2]);//0(2)
+}
+
+logFirstTwoBoxes(boxes);
+
+
